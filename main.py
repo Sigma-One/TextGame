5128326ba1f9c81432d2a37d1ad0c16b5406f142
Sigma-One <sarvikuonokas@luukku.com>
2017-11-26 13:37:14 +0200

    Updates to stuff, I can't think of commit messagees today.

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 #Just testing text based games again
+import util
 
 print("starting game")
 
-import inventory as inv
-print("Inventory loaded.")
-
 import stats
-print("Stats loaded.")
 
-def setLevel(level):
-	global lvl
-	lvl = __import__(level)
-
-setLevel("spawnArea")
+util.setLevel("spawnArea")
 
 while 1 == 1:	
         
         action = raw_input("\nWhat do you want to do? ")
         
-        lvl.act(action)
+        util.lvl.act(action)
         
         if stats.getHealth() < 1:
                 print("You died.")
---
+++ util.py
@@ -0,0 +1,3 @@
+def setLevel(level):
+	global lvl
+	lvl = __import__(level)
